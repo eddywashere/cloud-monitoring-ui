36d2628aa7718be991ccfd0fba0aeb1164780c7d
Eddy Hernandez <eddy.hernandez@rackspace.com>
2014-05-25 15:56:44 -0700

    show agent host info on detail page

--- app/scripts/controllers/entity-detail.js
+++ app/scripts/controllers/entity-detail.js
@@ -2,13 +2,27 @@
 
 angular.module('dashboardApp')
   .controller('EntityDetailCtrl', function ($scope, $routeParams, Entities) {
+
+    var getHostInfo = function(){
+      Entities.getAgentHostInfo($scope.entity.id).then(function(data){
+        $scope.entity.hostInfo = data.values[0].host_info;
+        $scope.hostInfoLoading = false;
+      }).catch(function(){
+        $scope.hostInfoLoading = false;
+      });
+    };
+
+    $scope.hostInfoLoading = true;
+
     if (Entities.currentEntity.id) {
       $scope.entity = Entities.currentEntity;
+      getHostInfo();
     } else {
       // make call to get entity
       Entities.getDetail($routeParams.entityid).then(function(data){
         $scope.entity = data;
         Entities.currentEntity = $scope.entity;
+        getHostInfo();
       });
     }
   });
--- app/scripts/services/entity.js
+++ app/scripts/services/entity.js
@@ -1,18 +1,48 @@
 'use strict';
 
 angular.module('dashboardApp')
-  .factory('Entities', function ($resource) {
+  .factory('Entities', function ($resource, $cacheFactory, $timeout) {
 
     var entities = {},
     Entities = $resource('/proxy/cloudMonitoring/entities/:entityId',
       {
         entityId: '@entityId'
+      },
+      {
+        'query': {
+          method: 'GET',
+          cache: $cacheFactory,
+          isArray: false
+        }
+      }),
+    AgentHostInfo = $resource('/proxy/cloudMonitoring/views/agent_host_info?entityId=:entityId&include=memory&include=cpus&include=disks&include=processes&include=network_interfaces',
+      {
+        entityId: '@entityId'
+      },
+      {
+        'get': {
+          method: 'GET',
+          isArray: false
+        }
       });
 
     entities.currentEntity = {};
 
+    var getListTimeout;
     entities.getList = function(){
-      return Entities.get().$promise;
+      if (!angular.isDefined(getListTimeout)) {
+        getListTimeout = $timeout(entities.clearListCache, 60000);
+      }
+      return Entities.query().$promise;
+    };
+
+    entities.clearListCache = function(){
+      $timeout.cancel(getListTimeout);
+      $cacheFactory.get('$http').remove('/proxy/cloudMonitoring/entities');
+    };
+
+    entities.create = function(){
+      entities.clearListCache();
     };
 
     entities.getDetail = function(entityId){
@@ -20,14 +50,10 @@ angular.module('dashboardApp')
         entityId: entityId
       }).$promise;
     };
-    // MonitoringAgentService.getAgentInstaller = function(entityId) {
-    //   var data = {
-    //     entity_id: entityId
-    //   };
 
-    //   return agentInstaller().post(data).promise;
-    // };
+    entities.getAgentHostInfo = function(entityId){
+      return AgentHostInfo.get({entityId: entityId}).$promise;
+    };
 
-    // Public API here
     return entities;
   });
